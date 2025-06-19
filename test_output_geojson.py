2682e04bdce84751f510b47eead82058f4a63756
Cheng-Jung <100653419+ChengJungHsu@users.noreply.github.com>
2025-06-19 14:46:45 +0800

    Update test_output_geojson.py

--- test_output_geojson.py
+++ test_output_geojson.py
@@ -16,9 +16,14 @@ import pytz
 from pyproj import CRS
 from coastsat import SDS_download, SDS_preprocess, SDS_shoreline, SDS_tools, SDS_transects, SDS_slope
 
+
 geojson_output_file = os.path.join(os.getcwd(), 'examples', 'tp2_L5_L7_S2_output_lines.geojson')
 settings = {'output_epsg': 3826}
+distance_threshold = 100
+
+#test1 - load shorelines from a .geojson file: SDS.output_from_geojson(filename)
 output = SDS_tools.output_from_geojson(geojson_output_file)
+#test2- output multilinestring type SDS_tools.output_to_gdf_multilines(output, distance_threshold)
 gdf = SDS_tools.output_to_gdf_multilines(output, distance_threshold)
 gdf.crs = CRS(settings['output_epsg']) # set layer projection
 # save GEOJSON layer to file
